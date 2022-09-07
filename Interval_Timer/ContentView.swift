f596b413b8ff494dbe372cdd289689ee2b08a186
Daisei Tanaka <d.tnk.4004@gmail.com>
2022-09-07 21:50:02 +0900

    Initial Commit

---
+++ Interval_Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Interval_Timer
+//
+//  Created by 田中大誓 on 2022/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Interval_Timer/Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerApp.swift
+//  Interval_Timer
+//
+//  Created by 田中大誓 on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct Interval_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
